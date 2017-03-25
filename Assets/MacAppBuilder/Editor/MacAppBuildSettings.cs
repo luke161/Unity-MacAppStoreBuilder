2faa250bf47c5ad8dc9ba03e7f5e840e54498b8b
Luke Holland <luke161@gmail.com>
2017-03-25 16:57:38 +0000

    Added project files

---
+++ Assets/MacAppBuilder/Editor/MacAppBuildSettings.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class MacAppBuildSettings : ScriptableObject
+{
+
+	public string applicationCategory = "public.app-category.games";
+	public string applicationDescription = "[YEAR] [YOUR COMPANY NAME]. All rights reserved.";
+
+	public string provisioningProfileApplication = "3rd Party Mac Developer Application: [YOUR COMPANY NAME]";
+	public string provisioningProfileInstaller = "3rd Party Mac Developer Installer: [YOUR COMPANY NAME]";
+
+	public Object playerIconsFile;
+	public Object entitlementsFile;
+
+	public string playerIconsPath {
+		get {
+			if(playerIconsFile==null) return null;
+			else {
+				return AssetDatabase.GetAssetPath(playerIconsFile);
+			}
+		}
+	}
+
+	public string entitlementsPath {
+		get {
+			if(entitlementsFile==null) return null;
+			else {
+				return AssetDatabase.GetAssetPath(entitlementsFile);
+			}
+		}
+	}
+
+}
+
+[CustomEditor(typeof(MacAppBuildSettings))]
+public class MacAppBuildSettingsEditor : Editor
+{
+
+	private SerializedProperty _propertyApplicationCategory;
+	private SerializedProperty _propertyApplicationDescription;
+	private SerializedProperty _propertyProvisioningProfileApplication;
+	private SerializedProperty _propertyProvisioningProfileInstaller;
+	private SerializedProperty _propertyFilePlayerIcons;
+	private SerializedProperty _propertyFileEntitlements;
+
+	public void OnEnable()
+	{
+		_propertyApplicationCategory = serializedObject.FindProperty("applicationCategory");
+		_propertyApplicationDescription = serializedObject.FindProperty("applicationDescription");
+		_propertyProvisioningProfileApplication = serializedObject.FindProperty("provisioningProfileApplication");
+		_propertyProvisioningProfileInstaller = serializedObject.FindProperty("provisioningProfileInstaller");
+		_propertyFilePlayerIcons = serializedObject.FindProperty("playerIconsFile");
+		_propertyFileEntitlements = serializedObject.FindProperty("entitlementsFile");
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.LabelField("Application Settings",EditorStyles.boldLabel);
+		EditorGUILayout.PropertyField(_propertyApplicationCategory,new GUIContent("Category"));
+		EditorGUILayout.PropertyField(_propertyApplicationDescription,new GUIContent("Description"));
+
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Provisioning Profiles",EditorStyles.boldLabel);
+		EditorGUILayout.PropertyField(_propertyProvisioningProfileApplication,new GUIContent("Application"));
+		EditorGUILayout.PropertyField(_propertyProvisioningProfileInstaller,new GUIContent("Installer"));
+
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Required Files",EditorStyles.boldLabel);
+		EditorGUILayout.PropertyField(_propertyFilePlayerIcons,new GUIContent("Player Icons"));
+		EditorGUILayout.PropertyField(_propertyFileEntitlements,new GUIContent("Entitlements"));
+
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+}
---
+++ Assets/MacAppBuilder/Editor/MacAppBuilder.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+
+public static class MacAppBuilder 
+{
+
+	private const string kSettingsPath = "Assets/MacAppBuilder/MacAppBuilderSettings.asset";
+
+	[MenuItem("Tools/MacOS/Build for Mac App Store")]
+	private static void MenuBuild()
+	{
+		string applicationPath = EditorUtility.OpenFilePanel("Select OSX Build","","app");
+		if(!string.IsNullOrEmpty(applicationPath)) Build(applicationPath);
+	}
+
+	[MenuItem("Tools/MacOS/Create Icon Set")]
+	private static void MenuCreateIconSet()
+	{
+		string iconsPath = EditorUtility.OpenFolderPanel("Select Icon Set Folder","","UnityPlayer.iconset");
+		if(!string.IsNullOrEmpty(iconsPath)) CreateIconSet(iconsPath);
+	}
+
+	public static void Build(string applicationPath)
+	{
+		string fileName = Path.GetFileName(applicationPath);
+		string applicationDirectory = Path.GetDirectoryName(applicationPath);
+
+		// load build settings
+		MacAppBuildSettings settings = AssetDatabase.LoadAssetAtPath<MacAppBuildSettings>(kSettingsPath);
+		if(settings==null){
+			settings = ScriptableObject.CreateInstance<MacAppBuildSettings>();
+			AssetDatabase.CreateAsset(settings,kSettingsPath);
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+
+		// copy player icons
+		if(!string.IsNullOrEmpty(settings.playerIconsPath)){
+			File.Copy(settings.playerIconsPath,Path.Combine(applicationPath,"Contents/Resources/PlayerIcon.icns"),true);
+		}
+
+		// update info.plist
+		string infoPlistPath = Path.Combine(applicationPath,"Contents/Info.plist");
+		PlistDocument info = new PlistDocument();
+		info.ReadFromFile(infoPlistPath);
+
+		PlistElementDict root = info.root;
+		root.SetString("CFBundleSignature",PlayerSettings.bundleIdentifier);
+		root.SetString("CFBundleIdentifier",PlayerSettings.bundleIdentifier);
+		root.SetString("CFBundleGetInfoString",settings.applicationDescription);
+		root.SetString("LSApplicationCategoryType",settings.applicationCategory);
+		root.SetString("CFBundleShortVersionString",PlayerSettings.bundleVersion);
+		root.SetString("CFBundleVersion",PlayerSettings.bundleVersion);
+
+		info.WriteToFile(infoPlistPath);
+
+		// copy entitlements
+		string entitlementsName = string.Concat(PlayerSettings.productName,".entitlements");
+		if(!string.IsNullOrEmpty(settings.entitlementsPath)){
+			File.Copy(settings.entitlementsPath,Path.Combine(applicationDirectory,entitlementsName),true);
+		} else {
+			throw new UnityException("Entitlements file required");
+		}
+
+		// run terminal commands
+		ProcessStartInfo startInfo = new ProcessStartInfo("/bin/bash");
+		startInfo.WorkingDirectory = applicationDirectory;
+		startInfo.UseShellExecute = false;
+		startInfo.RedirectStandardInput = true;
+		startInfo.RedirectStandardOutput = true;
+
+		Process process = new Process();
+		process.StartInfo = startInfo;
+		process.Start();
+
+		process.StandardInput.WriteLine(string.Format("chmod -R a+xr {0}",fileName));
+		process.StandardInput.WriteLine(string.Format("codesign --force --sign '{2}' --entitlements {0} {1} --deep",entitlementsName,fileName,settings.provisioningProfileApplication));
+		process.StandardInput.WriteLine(string.Format("productbuild --component {0} /Applications --sign '{2}' {1}.pkg",fileName,PlayerSettings.productName,settings.provisioningProfileInstaller));
+
+		process.StandardInput.WriteLine("exit");
+		process.StandardInput.Flush();
+
+		UnityEngine.Debug.Log(process.StandardOutput.ReadToEnd());
+
+		process.WaitForExit();
+	}
+
+	public static void CreateIconSet(string iconsPath)
+	{
+		string workingDirectory = Directory.GetParent( iconsPath ).ToString();
+
+		// run terminal commands
+		ProcessStartInfo startInfo = new ProcessStartInfo("/bin/bash");
+		startInfo.WorkingDirectory = workingDirectory;
+		startInfo.UseShellExecute = false;
+		startInfo.RedirectStandardInput = true;
+		startInfo.RedirectStandardOutput = true;
+
+		Process process = new Process();
+		process.StartInfo = startInfo;
+		process.Start();
+
+		process.StandardInput.WriteLine(string.Format("iconutil -c icns \"{0}\"",iconsPath));
+		process.StandardInput.WriteLine("exit");
+		process.StandardInput.Flush();
+
+		process.WaitForExit();
+
+		AssetDatabase.Refresh();
+	}
+
+}
